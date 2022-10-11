e974a3250ae2ff79d89c2504cc4a0dfcb5b0b3fa
Xavierl <xavier@voxowl.com>
2022-10-11 16:25:09 +0200

    added float3 tests

--- core/tests/test_list.c
+++ core/tests/test_list.c
@@ -7,10 +7,20 @@
 #include "acutest.h"
 
 #include "test_hash_uint32_int.h"
+#include "test_float3.h"
 #include "test_shape.h"
 
 TEST_LIST = {
     { "hash_uint32_int", test_hash_uint32_int },
+
+    // float3
+    { "float3_new(Number3)", test_float3_new },
+    { "float3_copy", test_float3_copy },
+    { "float3_const", test_float3_const },
+    { "float3_products", test_float3_products },
+    { "float3_length", test_float3_length },
+    { "float3_min_max", test_float3_min_max },
+    { "float3_operations", test_float3_operations },
 
     // shape
     { "test_shape_addblock_1", test_shape_addblock_1 },
