7fe9c2581318750c91411cb8c3aa6e2bd326438e
Corentin Cailleaud <corentin.cailleaud@caillef.com>
2022-10-26 15:23:03 +0200

    Added Resource tyme and serialize resources

---
+++ core/resource.h
@@ -0,0 +1,13 @@
+//
+//  resource.h
+//  Particubes
+//
+//  Created by Corentin Cailleaud on 25/10/2022.
+//
+
+#ifndef resource_h
+#define resource_h
+
+#include <stdio.h>
+
+#endif /* resource_h */
