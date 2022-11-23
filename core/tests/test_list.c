0271f02e3f012742e99c206856c373a239254970
Xavierl <xavier@voxowl.com>
2022-11-23 10:13:47 +0100

    shape unit tests

--- core/tests/test_list.c
+++ core/tests/test_list.c
@@ -47,114 +47,14 @@ TEST_LIST = {
     {"test_block_get_color_index", test_block_get_color_index},
     {"test_block_is_solid", test_block_is_solid},
     {"test_block_equal", test_block_equal},
-    // { "test_block_is_opaque", test_block_is_opaque },
-    // { "test_block_is_transparent", test_block_is_transparent },
-    // { "test_block_is_ao_and_light_caster", test_block_is_ao_and_light_caster },
-    // { "test_block_is_any", test_block_is_any },
     {"test_aware_block_get", test_aware_block_get},
     {"test_aware_block_new_copy", test_aware_block_new_copy},
     {"test_aware_block_set_touched_face", test_aware_block_set_touched_face},
     {"test_block_getNeighbourBlockCoordinates", test_block_getNeighbourBlockCoordinates},
 
-    // blockChange
-    {"test_blockChange_get", test_blockChange_get},
-    {"test_blockChange_amend", test_blockChange_amend},
-
-    // box
-    {"test_box_new", test_box_new},
-    {"test_box_new_2", test_box_new_2},
-    {"test_box_new_copy", test_box_new_copy},
-    {"test_box_set_bottom_center_position", test_box_set_bottom_center_position},
-    {"test_box_get_center", test_box_get_center},
-    {"test_box_copy", test_box_copy},
-    {"test_box_collide", test_box_collide},
-    {"test_box_contains", test_box_contains},
-    {"test_box_set_broadphase_box", test_box_set_broadphase_box},
-    {"test_box_get_size", test_box_get_size},
-    {"test_box_is_empty", test_box_is_empty},
-    {"test_box_squarify", test_box_squarify},
-    {"test_box_op_merge", test_box_op_merge},
-    {"test_box_get_volume", test_box_get_volume},
-    {"test_box_to_aabox_no_rot", test_box_to_aabox_no_rot},
-    {"test_box_to_aabox2", test_box_to_aabox2},
-
-    // chunk
-    {"test_chunk_new", test_chunk_new},
-    {"test_chunk_Block", test_chunk_Block},
-    {"test_chunk_needs_display", test_chunk_needs_display},
-
-    // config
-    {"test_upper_power_of_two", test_upper_power_of_two},
-
-    // doubly_linked_list_uint8
-    {"doubly_linked_list_uint8_new", test_doubly_linked_list_uint8_new},
-    {"doubly_linked_list_uint8_node_new", test_doubly_linked_list_uint8_node_new},
-    {"doubly_linked_list_uint8_node_get_value", test_doubly_linked_list_uint8_node_get_value},
-    {"doubly_linked_list_uint8_node_set_value", test_doubly_linked_list_uint8_node_set_value},
-    {"doubly_linked_list_uint8_push_front", test_doubly_linked_list_uint8_push_front},
-    {"doubly_linked_list_uint8_push_back", test_doubly_linked_list_uint8_push_back},
-    {"doubly_linked_list_uint8_node_next", test_doubly_linked_list_uint8_node_next},
-    {"doubly_linked_list_uint8_node_previous", test_doubly_linked_list_uint8_node_previous},
-    {"doubly_linked_list_uint8_node_count", test_doubly_linked_list_uint8_node_count},
-    {"doubly_linked_list_uint8_node_flush", test_doubly_linked_list_uint8_node_flush},
-    {"doubly_linked_list_uint8_contains", test_doubly_linked_list_uint8_contains},
-    {"doubly_linked_list_uint8_pop_front", test_doubly_linked_list_uint8_pop_front},
-    {"doubly_linked_list_uint8_pop_back", test_doubly_linked_list_uint8_pop_back},
-    {"doubly_linked_list_uint8_front", test_doubly_linked_list_uint8_front},
-    {"doubly_linked_list_uint8_back", test_doubly_linked_list_uint8_back},
-    {"doubly_linked_list_uint8_insert_node_before",
-     test_doubly_linked_list_uint8_insert_node_before},
-    {"doubly_linked_list_uint8_insert_node_after", test_doubly_linked_list_uint8_insert_node_after},
-    {"doubly_linked_list_uint8_delete_node", test_doubly_linked_list_uint8_delete_node},
-    // doubly_linked_list
-    {"doubly_linked_list_new", test_doubly_linked_list_new},
-    {"doubly_linked_list_node_new", test_doubly_linked_list_node_new},
-    {"doubly_linked_list_node_pointer", test_doubly_linked_list_node_pointer},
-    {"doubly_linked_list_node_set_pointer", test_doubly_linked_list_node_set_pointer},
-    {"doubly_linked_list_push_first", test_doubly_linked_list_push_first},
-    {"doubly_linked_list_push_last", test_doubly_linked_list_push_last},
-    {"doubly_linked_list_node_next", test_doubly_linked_list_node_next},
-    {"doubly_linked_list_node_previous", test_doubly_linked_list_node_previous},
-    {"doubly_linked_list_node_count", test_doubly_linked_list_node_count},
-    {"doubly_linked_list_flush", test_doubly_linked_list_flush},
-    {"doubly_linked_list_contains", test_doubly_linked_list_contains},
-    {"doubly_linked_list_pop_first", test_doubly_linked_list_pop_first},
-    {"doubly_linked_list_pop_back", test_doubly_linked_list_pop_last},
-    {"doubly_linked_list_first", test_doubly_linked_list_first},
-    {"doubly_linked_list_last", test_doubly_linked_list_last},
-    {"doubly_linked_list_insert_node_next", test_doubly_linked_list_insert_node_next},
-    {"doubly_linked_list_insert_node_previous", test_doubly_linked_list_insert_node_previous},
-    {"doubly_linked_list_delete_node", test_doubly_linked_list_delete_node},
-    {"doubly_linked_list_node_at_index", test_doubly_linked_list_node_at_index},
-    {"doubly_linked_list_sort_ascending", test_doubly_linked_list_sort_ascending},
-
-    // fifo_list
-    {"fifo_list_new", test_fifo_list_new},
-    {"fifo_list_flush", test_fifo_list_flush},
-    {"fifo_list_get_size", test_fifo_list_get_size},
-    {"fifo_list_pop", test_fifo_list_pop},
-    {"fifo_list_push", test_fifo_list_push},
-    {"fifo_list_new_copy", test_fifo_list_new_copy},
-    // filo_list
-    {"filo_list_new", test_filo_list_new},
-    {"filo_list_push", test_filo_list_push},
-    {"filo_list_pop", test_filo_list_pop},
-
-    // filo_list_float3
-    {"filo_list_float3_pop", test_filo_list_float3_pop},
-    {"filo_list_float3_recycle", test_filo_list_float3_recycle},
-
     // filo_list_int3
     {"filo_list_int3_pop", test_filo_list_int3_pop},
     {"filo_list_int3_recycle", test_filo_list_int3_recycle},
-
-    // filo_list_uint16
-    {"test_filo_list_uint16_push", test_filo_list_uint16_push},
-    {"test_filo_list_uint16_pop", test_filo_list_uint16_pop},
-
-    // filo_list_uint32
-    {"test_filo_list_uint32_push", test_filo_list_uint32_push},
-    {"test_filo_list_uint32_pop", test_filo_list_uint32_pop},
 
     // float3
     {"float3_new(Number3)", test_float3_new},
@@ -227,20 +127,6 @@ TEST_LIST = {
     {"int3_op_max", test_int3_op_max},
     {"int3_op_div_ints", test_int3_op_div_ints},
 
-    // map_string_float3
-    {"map_string_float3_new", test_map_string_float3_new},
-    {"map_string_float3_iterator_new", test_map_string_float3_iterator_new},
-    {"map_string_float3_set_key_value", test_map_string_float3_set_key_value},
-    {"map_string_float3_iterator_next", test_map_string_float3_iterator_next},
-    {"map_string_float3_iterator_current_key", test_map_string_float3_iterator_current_key},
-    {"map_string_float3_iterator_current_value", test_map_string_float3_iterator_current_value},
-    {"map_string_float3_iterator_replace_current_value",
-     test_map_string_float3_iterator_replace_current_value},
-    {"map_string_float3_iterator_is_done", test_map_string_float3_iterator_is_done},
-    {"map_string_float3_value_for_key", test_map_string_float3_value_for_key},
-    {"map_string_mutable_float3_value_for_key", test_map_string_mutable_float3_value_for_key},
-    {"map_string_float3_remove_key", test_map_string_float3_remove_key},
-
     // matrix4x4
     {"matrix4x4_new", test_matrix4x4_new},
     {"matrix4x4_copy", test_matrix4x4_new_copy},
@@ -291,32 +177,6 @@ TEST_LIST = {
     {"quaternion_rotate_vector", test_quaternion_rotate_vector},
     {"quaternion_coherence_check", test_quaternion_coherence_check},
 
-    // rtree
-    {"rtree_new", test_rtree_new},
-    {"rtree_node_get_aabb", test_rtree_node_get_aabb},
-    {"rtree_node_get_groups", test_rtree_node_get_groups},
-    {"rtree_node_get_collides_with", test_rtree_node_get_collides_with},
-    {"rtree_create_and_insert", test_rtree_create_and_insert},
-
-    // shape
-    {"test_shape_addblock_1", test_shape_addblock_1},
-    // { "test_shape_addblock_2", test_shape_addblock_2 },
-    // { "test_shape_addblock_3", test_shape_addblock_3 },
-
-    // stream
-    {"stream_new_buffer_read", test_stream_new_buffer_read},
-    {"stream_new_file_read", test_stream_new_file_read},
-    {"stream_read", test_stream_read},
-    {"stream_read_uint8", test_stream_read_uint8},
-    {"stream_read_uint16", test_stream_read_uint16},
-    {"stream_read_uint32", test_stream_read_uint32},
-    {"stream_read_float32", test_stream_read_float32},
-    {"stream_read_string", test_stream_read_string},
-    {"stream_skip", test_stream_skip},
-    {"stream_get_cursor_position", test_stream_get_cursor_position},
-    {"stream_set_cursor_position", test_stream_set_cursor_position},
-    {"stream_reached_the_end", test_stream_reached_the_end},
-
     // transaction
     {"transaction_new", test_transaction_new},
     {"transaction_getCurrentBlockAt", test_transaction_getCurrentBlockAt},
@@ -348,6 +208,10 @@ TEST_LIST = {
     {"test_utils_stringArray_n_append", test_utils_stringArray_n_append},
     {"test_utils_string_split", test_utils_string_split},
 
+    // filo_list_float3
+    {"filo_list_float3_pop", test_filo_list_float3_pop},
+    {"filo_list_float3_recycle", test_filo_list_float3_recycle},
+
     // weakptr
     {"weakptr_new", test_weakptr_new},
     {"weakptr_retain", test_weakptr_retain},
@@ -355,6 +219,156 @@ TEST_LIST = {
     {"weakptr_get", test_weakptr_get},
     {"weakptr_get_or_release", test_weakptr_get_or_release},
     {"weakptr_invalidate", test_weakptr_invalidate},
+
+    // filo_list_uint16
+    {"test_filo_list_uint16_push", test_filo_list_uint16_push},
+    {"test_filo_list_uint16_pop", test_filo_list_uint16_pop},
+
+    // blockChange
+    {"test_blockChange_get", test_blockChange_get},
+    {"test_blockChange_amend", test_blockChange_amend},
+
+    // box
+    {"test_box_new", test_box_new},
+    {"test_box_new_2", test_box_new_2},
+    {"test_box_new_copy", test_box_new_copy},
+    {"test_box_set_bottom_center_position", test_box_set_bottom_center_position},
+    {"test_box_get_center", test_box_get_center},
+    {"test_box_copy", test_box_copy},
+    {"test_box_collide", test_box_collide},
+    {"test_box_contains", test_box_contains},
+    {"test_box_set_broadphase_box", test_box_set_broadphase_box},
+    {"test_box_get_size", test_box_get_size},
+    {"test_box_is_empty", test_box_is_empty},
+    {"test_box_squarify", test_box_squarify},
+    {"test_box_op_merge", test_box_op_merge},
+    {"test_box_get_volume", test_box_get_volume},
+    {"test_box_to_aabox_no_rot", test_box_to_aabox_no_rot},
+    {"test_box_to_aabox2", test_box_to_aabox2},
+
+    // chunk
+    {"test_chunk_new", test_chunk_new},
+    {"test_chunk_Block", test_chunk_Block},
+    {"test_chunk_needs_display", test_chunk_needs_display},
+
+    // config
+    {"test_upper_power_of_two", test_upper_power_of_two},
+
+    // doubly_linked_list_uint8
+    {"doubly_linked_list_uint8_new", test_doubly_linked_list_uint8_new},
+    {"doubly_linked_list_uint8_node_new", test_doubly_linked_list_uint8_node_new},
+    {"doubly_linked_list_uint8_node_get_value", test_doubly_linked_list_uint8_node_get_value},
+    {"doubly_linked_list_uint8_node_set_value", test_doubly_linked_list_uint8_node_set_value},
+    {"doubly_linked_list_uint8_push_front", test_doubly_linked_list_uint8_push_front},
+    {"doubly_linked_list_uint8_push_back", test_doubly_linked_list_uint8_push_back},
+    {"doubly_linked_list_uint8_node_next", test_doubly_linked_list_uint8_node_next},
+    {"doubly_linked_list_uint8_node_previous", test_doubly_linked_list_uint8_node_previous},
+    {"doubly_linked_list_uint8_node_count", test_doubly_linked_list_uint8_node_count},
+    {"doubly_linked_list_uint8_node_flush", test_doubly_linked_list_uint8_node_flush},
+    {"doubly_linked_list_uint8_contains", test_doubly_linked_list_uint8_contains},
+    {"doubly_linked_list_uint8_pop_front", test_doubly_linked_list_uint8_pop_front},
+    {"doubly_linked_list_uint8_pop_back", test_doubly_linked_list_uint8_pop_back},
+    {"doubly_linked_list_uint8_front", test_doubly_linked_list_uint8_front},
+    {"doubly_linked_list_uint8_back", test_doubly_linked_list_uint8_back},
+    {"doubly_linked_list_uint8_insert_node_before",
+     test_doubly_linked_list_uint8_insert_node_before},
+    {"doubly_linked_list_uint8_insert_node_after", test_doubly_linked_list_uint8_insert_node_after},
+    {"doubly_linked_list_uint8_delete_node", test_doubly_linked_list_uint8_delete_node},
+
+    // doubly_linked_list
+    {"doubly_linked_list_new", test_doubly_linked_list_new},
+    {"doubly_linked_list_node_new", test_doubly_linked_list_node_new},
+    {"doubly_linked_list_node_pointer", test_doubly_linked_list_node_pointer},
+    {"doubly_linked_list_node_set_pointer", test_doubly_linked_list_node_set_pointer},
+    {"doubly_linked_list_push_first", test_doubly_linked_list_push_first},
+    {"doubly_linked_list_push_last", test_doubly_linked_list_push_last},
+    {"doubly_linked_list_node_next", test_doubly_linked_list_node_next},
+    {"doubly_linked_list_node_previous", test_doubly_linked_list_node_previous},
+    {"doubly_linked_list_node_count", test_doubly_linked_list_node_count},
+    {"doubly_linked_list_flush", test_doubly_linked_list_flush},
+    {"doubly_linked_list_contains", test_doubly_linked_list_contains},
+    {"doubly_linked_list_pop_first", test_doubly_linked_list_pop_first},
+    {"doubly_linked_list_pop_back", test_doubly_linked_list_pop_last},
+    {"doubly_linked_list_first", test_doubly_linked_list_first},
+    {"doubly_linked_list_last", test_doubly_linked_list_last},
+    {"doubly_linked_list_insert_node_next", test_doubly_linked_list_insert_node_next},
+    {"doubly_linked_list_insert_node_previous", test_doubly_linked_list_insert_node_previous},
+    {"doubly_linked_list_delete_node", test_doubly_linked_list_delete_node},
+    {"doubly_linked_list_node_at_index", test_doubly_linked_list_node_at_index},
+    {"doubly_linked_list_sort_ascending", test_doubly_linked_list_sort_ascending},
+
+    // fifo_list
+    {"fifo_list_new", test_fifo_list_new},
+    {"fifo_list_flush", test_fifo_list_flush},
+    {"fifo_list_get_size", test_fifo_list_get_size},
+    {"fifo_list_pop", test_fifo_list_pop},
+    {"fifo_list_push", test_fifo_list_push},
+    {"fifo_list_new_copy", test_fifo_list_new_copy},
+
+    // filo_list
+    {"filo_list_new", test_filo_list_new},
+    {"filo_list_push", test_filo_list_push},
+    {"filo_list_pop", test_filo_list_pop},
+
+    // filo_list_uint32
+    {"test_filo_list_uint32_push", test_filo_list_uint32_push},
+    {"test_filo_list_uint32_pop", test_filo_list_uint32_pop},
+
+    // map_string_float3
+    {"map_string_float3_new", test_map_string_float3_new},
+    {"map_string_float3_iterator_new", test_map_string_float3_iterator_new},
+    {"map_string_float3_set_key_value", test_map_string_float3_set_key_value},
+    {"map_string_float3_iterator_next", test_map_string_float3_iterator_next},
+    {"map_string_float3_iterator_current_key", test_map_string_float3_iterator_current_key},
+    {"map_string_float3_iterator_current_value", test_map_string_float3_iterator_current_value},
+    {"map_string_float3_iterator_replace_current_value",
+     test_map_string_float3_iterator_replace_current_value},
+    {"map_string_float3_iterator_is_done", test_map_string_float3_iterator_is_done},
+    {"map_string_float3_value_for_key", test_map_string_float3_value_for_key},
+    {"map_string_mutable_float3_value_for_key", test_map_string_mutable_float3_value_for_key},
+    {"map_string_float3_remove_key", test_map_string_float3_remove_key},
+
+    // rtree
+    {"rtree_new", test_rtree_new},
+    {"rtree_node_get_aabb", test_rtree_node_get_aabb},
+    {"rtree_node_get_groups", test_rtree_node_get_groups},
+    {"rtree_node_get_collides_with", test_rtree_node_get_collides_with},
+    {"rtree_create_and_insert", test_rtree_create_and_insert},
+
+    // shape
+    {"shape_make", test_shape_make},
+    {"shape_make_copy", test_shape_make_copy},
+    {"shape_make_with_fixed_size", test_shape_make_with_fixed_size},
+    {"shape_make_with_octree", test_shape_make_with_octree},
+    {"shape_set_transform", test_shape_set_transform},
+    {"shape_retain", test_shape_retain},
+    {"shape_release", test_shape_release},
+    {"shape_get_id", test_shape_get_id},
+    {"shape_get_palette", test_shape_get_palette},
+    {"shape_remove_block", test_shape_remove_block},
+    {"shape_get_bounding_box_size", test_shape_get_bounding_box_size},
+    {"shape_get_fixed_size", test_shape_get_fixed_size},
+    {"shape_is_within_fixed_bounds", test_shape_is_within_fixed_bounds},
+    {"shape_get_model_aabb", test_shape_get_model_aabb},
+    {"shape_set_fullname", test_shape_set_fullname},
+    {"shape_get_fullname", test_shape_get_fullname},
+    {"test_shape_addblock_1", test_shape_addblock_1},
+    // {"test_shape_addblock_2", test_shape_addblock_2},
+    {"test_shape_addblock_3", test_shape_addblock_3},
+
+    // stream
+    {"stream_new_buffer_read", test_stream_new_buffer_read},
+    {"stream_new_file_read", test_stream_new_file_read},
+    {"stream_read", test_stream_read},
+    {"stream_read_uint8", test_stream_read_uint8},
+    {"stream_read_uint16", test_stream_read_uint16},
+    {"stream_read_uint32", test_stream_read_uint32},
+    {"stream_read_float32", test_stream_read_float32},
+    {"stream_read_string", test_stream_read_string},
+    {"stream_skip", test_stream_skip},
+    {"stream_get_cursor_position", test_stream_get_cursor_position},
+    {"stream_set_cursor_position", test_stream_set_cursor_position},
+    {"stream_reached_the_end", test_stream_reached_the_end},
 
     {NULL, NULL} /* zeroed record marking the end of the list */
 };
