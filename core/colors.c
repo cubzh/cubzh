dc2251d603cbac7a10bd9b77f7fea877ca038673
Gaëtan de Villèle <gdevillele@gmail.com>
2023-01-02 21:16:49 +0100

    fix format

--- core/colors.c
+++ core/colors.c
@@ -13,27 +13,25 @@
 //
 // --------------------------------------------------
 
-hsv rgb2hsv(rgb in)
-{
-    hsv         out;
-    double      min, max, delta;
+hsv rgb2hsv(rgb in) {
+    hsv out;
+    double min, max, delta;
 
     min = in.r < in.g ? in.r : in.g;
-    min = min  < in.b ? min  : in.b;
+    min = min < in.b ? min : in.b;
 
     max = in.r > in.g ? in.r : in.g;
-    max = max  > in.b ? max  : in.b;
+    max = max > in.b ? max : in.b;
 
-    out.v = max;                                // v
+    out.v = max; // v
     delta = max - min;
-    if (delta < 0.00001)
-    {
+    if (delta < 0.00001) {
         out.s = 0;
         out.h = 0; // undefined, maybe nan?
         return out;
     }
-    if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
-        out.s = (delta / max);                  // s
+    if (max > 0.0) {           // NOTE: if Max is == 0, this divide would cause a crash
+        out.s = (delta / max); // s
     } else {
         // if max is 0, then r = g = b = 0
         // s = 0, h is undefined (0 arbitrary)
@@ -41,17 +39,16 @@ hsv rgb2hsv(rgb in)
         out.h = 0.0;
         return out;
     }
-    if( in.r >= max )                           // > is bogus, just keeps compilor happy
-        out.h = ( in.g - in.b ) / delta;        // between yellow & magenta
+    if (in.r >= max)                   // > is bogus, just keeps compilor happy
+        out.h = (in.g - in.b) / delta; // between yellow & magenta
+    else if (in.g >= max)
+        out.h = 2.0 + (in.b - in.r) / delta; // between cyan & yellow
     else
-    if( in.g >= max )
-        out.h = 2.0 + ( in.b - in.r ) / delta;  // between cyan & yellow
-    else
-        out.h = 4.0 + ( in.r - in.g ) / delta;  // between magenta & cyan
+        out.h = 4.0 + (in.r - in.g) / delta; // between magenta & cyan
 
-    out.h *= 60.0;                              // degrees
+    out.h *= 60.0; // degrees
 
-    if( out.h < 0.0 )
+    if (out.h < 0.0)
         out.h += 360.0;
 
     return out;
