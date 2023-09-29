72835ead883177e396d523bab3b04114c21de10b
SysFAB64 <97438817+FabberXD@users.noreply.github.com>
2023-09-29 19:51:23 +0300

    Update box_outline.lua

--- lua/modules/box_outline.lua
+++ lua/modules/box_outline.lua
@@ -4,11 +4,11 @@
 -- Place a red outline around the head of the player
 local boxOutline = require("box_outline")
 
-local size = Number3(Player.Head.Width + 1,Player.Head.Height + 1,Player.Head.Depth + 1)
+local size = Number3(Player.Head.Width + 1, Player.Head.Height + 1, Player.Head.Depth + 1)
 local thickness = 1
-local c = boxOutline:create(size,thickness,Color.Red)
+local c = boxOutline:create(size, thickness, Color.Red)
 c:SetParent(Player.Head)
-c.LocalPosition = -Player.Head.Pivot - Number3(0.5,0.5,0.5)
+c.LocalPosition = -Player.Head.Pivot - Number3(0.5, 0.5, 0.5)
 
 ]]--
 
@@ -28,17 +28,17 @@ local boxOutlineModuleMetatable = {
 				o.color = color
 
 				local segments = {
-					{ scaleField="X", pos={0,0,0} }, 			-- bottom front
-					{ scaleField="X", pos={0,0,size.Z} },  	  -- bottom back
-					{ scaleField="Z", pos={0,0,0} }, 			-- bottom left
-					{ scaleField="Z", pos={size.X,0,0} },  	  -- bottom right
-					{ scaleField="X", pos={0,size.Y,0} }, 	   -- top front
+					{ scaleField="X", pos={0,0,0} },   -- bottom front
+					{ scaleField="X", pos={0,0,size.Z} },   -- bottom back
+					{ scaleField="Z", pos={0,0,0} },   -- bottom left
+					{ scaleField="Z", pos={size.X,0,0} },   -- bottom right
+					{ scaleField="X", pos={0,size.Y,0} },   -- top front
 					{ scaleField="X", pos={0,size.Y,size.Z} },   -- top back
-					{ scaleField="Z", pos={0,size.Y,0} },    	-- top left
+					{ scaleField="Z", pos={0,size.Y,0} },   -- top left
 					{ scaleField="Z", pos={size.X,size.Y,0} },   -- top right
-					{ scaleField="Y", pos={0,0,0} }, 	  	  -- v left front
-					{ scaleField="Y", pos={0,0,size.Z} },   	 -- v left back
-					{ scaleField="Y", pos={size.X,0,0} },    	-- v right front
+					{ scaleField="Y", pos={0,0,0} },   -- v left front
+					{ scaleField="Y", pos={0,0,size.Z} },   -- v left back
+					{ scaleField="Y", pos={size.X,0,0} },   -- v right front
 					{ scaleField="Y", pos={size.X,0,size.Z} },   -- v right back
 				}
 				for k,segment in ipairs(segments) do
