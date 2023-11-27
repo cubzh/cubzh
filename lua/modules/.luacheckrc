c599dd2a156596a252147f33854144745a7a00da
Adrien Duermael <adrien@duermael.com>
2023-11-27 18:13:43 +0100

    walk_sfx: fixes and optims for distant players

--- lua/modules/.luacheckrc
+++ lua/modules/.luacheckrc
@@ -24,6 +24,7 @@ return {
 		"Screen",
 		"Sky",
 		"System",
+		"CollisionGroups",
 	},
 
 	read_globals = {
--- lua/modules/walk_sfx.lua
+++ lua/modules/walk_sfx.lua
@@ -13,48 +13,69 @@ DEFAULT_CONFIG = {
 	},
 	stepDelay = 0.3,
 	volume = 0.25,
+	radius = 200,
 }
 
 objects = {} -- configs, indexed by object ref
 
 local motion = Number3.Zero
+local distance
+local cgroup
+
 LocalEvent:Listen(LocalEvent.Name.Tick, function(dt)
 	for object, config in pairs(objects) do
-		motion:Set(object.Motion.X, 0, object.Motion.Z)
-		if object.Motion.SquaredLength > 0 and object.IsOnGround then
-			config.t = config.t + dt
-			if config.t > config.stepDelay then
-				config.t = config.t % config.stepDelay
+		distance = AudioListener.Position - object.Position
 
-				local impact = Ray(Player.Position + { 0, 0.1, 0 }, { 0, -1, 0 }):Cast(object.CollidesWithGroups)
-				local block = impact.Block
+		if distance.SquaredLength < config.radiusSquared then
+			motion:Set(object.Motion.X, 0, object.Motion.Z)
 
-				if block == nil then
-					return
-				end
+			if object.Motion.SquaredLength > 0 then -- and object.IsOnGround then
+				config.t = config.t + dt
 
-				local color = block.Color
-				local colorN3 = Number3(color.R, color.G, color.B)
-				colorN3:Normalize()
+				if config.t > config.stepDelay then
+					config.t = config.t % config.stepDelay
 
-				local surfaceName = "grass"
-
-				local dot = -1
-
-				local d
-				for surface, vector in pairs(config.colorsN3) do
-					d = colorN3:Dot(vector)
-					if d > dot then
-						surfaceName = surface
-						dot = d
+					-- supporting multi module parentBox system
+					if object.parentBox then
+						cgroup = object.parentBox.CollidesWithGroups
+					else
+						cgroup = object.CollidesWithGroups
 					end
-				end
 
-				local fileNum = math.random(5)
-				sfx("walk_" .. surfaceName .. "_" .. fileNum, { Position = object.Position, Volume = config.volume })
+					local impact = Ray(object.Position + { 0, 0.1, 0 }, { 0, -1, 0 }):Cast(cgroup)
+					local block = impact.Block
+
+					if block == nil then
+						return
+					end
+
+					local color = block.Color
+					local colorN3 = Number3(color.R, color.G, color.B)
+					colorN3:Normalize()
+
+					local surfaceName = "grass"
+
+					local dot = -1
+
+					local d
+					for surface, vector in pairs(config.colorsN3) do
+						d = colorN3:Dot(vector)
+						if d > dot then
+							surfaceName = surface
+							dot = d
+						end
+					end
+
+					local fileNum = math.random(5)
+
+					sfx(
+						"walk_" .. surfaceName .. "_" .. fileNum,
+						{ Position = object.Position, Volume = config.volume, Radius = config.radius }
+					)
+				end
+			elseif config.t > 0 then
+				config.t = config.stepDelay -- to play step as soon as walk resumes
 			end
-		elseif config.t > 0 then
-			config.t = config.stepDelay -- to play step as soon as walk resumes
 		end
 	end
 end)
@@ -72,6 +93,8 @@ walkSFX.register = function(_, object, config)
 	for name, color in pairs(config.colors) do
 		config.colorsN3[name] = Number3(color.R, color.G, color.B):Normalize()
 	end
+
+	config.radiusSquared = config.radius * config.radius
 
 	config.t = 0.0
 
