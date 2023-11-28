2211dfc25be0c54c3b3777e3f046795a7c227783
Adrien Duermael <adrien@duermael.com>
2023-11-28 11:59:02 +0100

    hold press actions

--- lua/modules/cubzh.lua
+++ lua/modules/cubzh.lua
@@ -20,6 +20,7 @@ local SPAWN_IN_BLOCK = Number3(107, 14, 73)
 local TITLE_SCREEN_CAMERA_POSITION_IN_BLOCK = Number3(107, 20, 73)
 local ROTATING_CAMERA_MAX_OFFSET_Y_IN_BLOCk = 2.0
 local REQUEST_FAIL_RETRY_DELAY = 5.0
+local HOLDING_TIME = 0.6 -- time to trigger action when holding button pressed
 
 local JUMP_VELOCITY = 82
 local MAX_AIR_JUMP_VELOCITY = 85
@@ -46,11 +47,15 @@ Client.OnStart = function()
 
 	addCollectibles()
 
+	-- createDraft((SPAWN_IN_BLOCK + { 0, 2, 0 }) * MAP_SCALE, 50, 50, 200, 600)
+	-- createDraft(Number3(107, 36, 0) * MAP_SCALE, 50, 50, 200, 600)
+
 	-- CONTROLS
 	-- Disabling controls until user is authenticated
 	directionalPad = Client.DirectionalPad -- default dir pad function backup
 	Client.DirectionalPad = nil
 	Client.Action1 = nil
+	Client.Action1Release = nil
 
 	-- set icon for action1 button (for touch screens)
 	local controls = require("controls")
@@ -99,7 +104,8 @@ Client.OnStart = function()
 
 	Menu:OnAuthComplete(function()
 		Client.DirectionalPad = directionalPad
-		Client.Action1 = jump
+		Client.Action1 = action1
+		Client.Action1Release = action1Release
 
 		showLocalPlayer()
 		print(Player.Username .. " joined!")
@@ -127,6 +133,9 @@ Client.OnStart = function()
 	-- SYNCED ACTIONS
 	multi:onAction("swingRight", function(sender)
 		sender:SwingRight()
+	end)
+	multi:onAction("equipGlider", function(sender)
+		sender:EquipBackpack(bundle.Shape("voxels.glider_backpack"))
 	end)
 end
 
@@ -239,6 +248,11 @@ function loadMap()
 				end)
 				if ok then
 					loadedObjects[objInfo.fullname] = o
+					-- print("loaded " .. objInfo.fullname)
+					o.Physics = objInfo.Physics or PhysicsMode.StaticPerBlock
+					if string.find(objInfo.fullname, "vines") or string.find(objInfo.fullname, "grass") then
+						o.Physics = PhysicsMode.Disabled
+					end
 				else
 					loadedObjects[objInfo.fullname] = "ERROR"
 					-- print("could not load " .. objInfo.fullname)
@@ -271,7 +285,7 @@ function loadMap()
 
 				obj.Pivot = Number3(obj.Width / 2, k.Min.Y + obj.Pivot.Y, obj.Depth / 2)
 				hierarchyactions:applyToDescendants(obj, { includeRoot = true }, function(l)
-					l.Physics = objInfo.Physics or PhysicsMode.StaticPerBlock
+					l.Physics = o.Physics
 					if turnOnShadows then
 						l.Shadow = true
 					end
@@ -287,9 +301,28 @@ function loadMap()
 	end
 end
 
-function jump()
+holdTimer = nil
+
+function action1()
 	objectSkills.jump(Player)
 	-- Dev:CopyToClipboard("" .. Player.Position.X .. ", " .. Player.Position.Y .. ", " .. Player.Position.Z)
+
+	holdTimer = Timer(HOLDING_TIME, function()
+		holdTimer = nil
+		if equipment == "" then
+			return
+		end
+		if equipment == "glider" then
+			print("GLIDE! (TODO)")
+			multi:action("equipGlider")
+		end
+	end)
+end
+
+function action1Release()
+	if holdTimer ~= nil then
+		holdTimer:Cancel()
+	end
 end
 
 function dropPlayer(p)
@@ -311,7 +344,20 @@ end
 
 -- collected part IDs (arrays)
 collectedGliderParts = {} -- {1, 3, 5}
-collectedJetpackParts = {}
+-- collectedJetpackParts = {}
+
+gliderBackpackCollectibles = {}
+gliderUnlocked = false
+
+equipment = nil
+
+function unlockGlider()
+	print("GLIDER AVAILABLE!")
+	gliderUnlocked = true
+	for _, backpack in ipairs(gliderBackpackCollectibles) do
+		backpack.object.PrivateDrawMode = 0
+	end
+end
 
 function addCollectibles()
 	local function spawnCollectibles()
@@ -334,28 +380,40 @@ function addCollectibles()
 			{ ID = 10, Position = Number3(453, 472, 156) },
 		}
 
-		local gliderBackpacks = {
-			{ Position = Number3(451, 102, 510) },
-		}
-
-		local backpackConfig = {
+		local defaultBackpackConfig = {
 			scale = 0.75,
 			rotation = Number3.Zero, -- { math.pi / 6, 0, math.pi / 6 },
 			position = Number3.Zero,
 			itemName = "voxels.glider_backpack",
 			onCollisionBegin = function(c)
-				print("NOT AVAILABLE YET")
+				if gliderUnlocked then
+					collectParticles.Position = c.object.Position
+					collectParticles:spawn(20)
+					sfx("wood_impact_3", { Position = c.object.Position, Volume = 0.6, Pitch = 1.3 })
+					Client:HapticFeedback()
+					collectible:remove(c)
+
+					Player:EquipBackpack(c.object)
+					equipment = "glider"
+				else
+					print("NOT AVAILABLE YET")
+				end
 			end,
 		}
 
-		for _, v in ipairs(gliderBackpacks) do
-			local config = conf:merge(backpackConfig, { position = v.Position })
-			collectible:create(config)
+		local gliderBackpackConfigs = {
+			{ position = Number3(451, 102, 510) },
+		}
+
+		for _, backpackConfig in ipairs(gliderBackpackConfigs) do
+			local config = conf:merge(defaultBackpackConfig, backpackConfig)
+			local c = collectible:create(config)
+			c.object.PrivateDrawMode = 1
+			table.insert(gliderBackpackCollectibles, c)
 		end
 
-		if #collectedGliderParts >= #gliderParts then
-			-- baseControls.enableGlider(Player, true)
-			print("GLIDER AVAILABLE!")
+		if #collectedGliderParts >= #gliderParts then -- or true then
+			unlockGlider()
 		else
 			local gliderPartConfig = {
 				scale = 0.5,
@@ -395,13 +453,12 @@ function addCollectibles()
 					end
 
 					if #collectedGliderParts >= #gliderParts then
-						print("GLIDER AVAILABLE!")
-						-- 	baseControls.enableGlider(Player, true)
-						-- 	baseControls.selectBackpack(Player, "glider")
-						-- 	if printDebug then
-						-- 		print("Glider Unlocked!")
-						-- 	end
+						unlockGlider()
 					end
+
+					-- if #collectedGliderParts == 1 then
+					-- 	unlockGlider()
+					-- end
 				end,
 			}
 			for _, v in ipairs(gliderParts) do
@@ -416,14 +473,15 @@ function addCollectibles()
 	local t = {}
 	t.get = function()
 		local store = KeyValueStore(Player.UserID)
-		store:get("collectedGliderParts", "collectedJetpackParts", function(ok, results)
+		-- store:get("collectedGliderParts", "collectedJetpackParts", function(ok, results)
+		store:get("collectedGliderParts", function(ok, results)
 			if ok then
 				if results.collectedGliderParts ~= nil then
-					-- collectedGliderParts = results.collectedGliderParts
+					collectedGliderParts = results.collectedGliderParts
 				end
-				if results.collectedJetpackParts ~= nil then
-					collectedJetpackParts = results.collectedJetpackParts
-				end
+				-- if results.collectedJetpackParts ~= nil then
+				-- 	collectedJetpackParts = results.collectedJetpackParts
+				-- end
 				spawnCollectibles()
 			else
 				Timer(REQUEST_FAIL_RETRY_DELAY, t.get)
@@ -431,4 +489,71 @@ function addCollectibles()
 		end)
 	end
 	t.get()
+end
+
+function createDraft(pos, width, depth, height, strength)
+	local o = Object()
+	o:SetParent(World)
+	o.Physics = PhysicsMode.Trigger
+	o.CollisionGroups = { 4 }
+
+	o.emitter = particles:newEmitter({
+		life = function()
+			return 0.6
+		end,
+		position = function()
+			return Number3(math.random(0, width), 0, math.random(0, depth))
+		end,
+		color = function()
+			return Color(255, 255, 255, 80)
+		end,
+		physics = function()
+			return true
+		end,
+		velocity = function()
+			return Number3(0, math.random(300, 400), 0)
+		end,
+		collidesWithGroups = function()
+			return {}
+		end,
+	})
+
+	o.emitter:SetParent(o)
+
+	o.as = AudioSource("wind_wind_child_1")
+	o.as:SetParent(o)
+	o.as.Volume = 0.8
+	o.as.Pitch = 1.2
+	o.as.Loop = true
+
+	o.Tick = function(self, _)
+		self.emitter:spawn(1)
+	end
+
+	o.LocalPosition = pos
+	o.CollisionBox = Box({ 0, 0, 0 }, { width, height, depth })
+	o.CollidesWithGroups = Player.CollisionGroups
+
+	-- o.OnCollisionBegin = function(self, other)
+	-- 	if other == Player and Player.isUsingGlider then
+	-- 		self.as:Play()
+	-- 	end
+	-- end
+
+	-- o.OnCollision = function(self, other)
+	-- 	if other == Player and Player.isUsingGlider then
+	-- 		Player.draftVelocity = strength
+	-- 	end
+	-- end
+
+	-- o.OnCollisionEnd = function(self, other)
+	-- 	if other == Player then
+	-- 		Player.draftVelocity = 0
+	-- 		Timer(1, function()
+	-- 			self.as:Stop()
+	-- 		end)
+	-- 	end
+	-- end
+
+	return o
 end
