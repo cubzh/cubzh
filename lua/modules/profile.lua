8d3719e3a91ba910c5f211d8f02f107ac2fc0af6
Gaëtan de Villèle <gdevillele@gmail.com>
2023-12-08 17:41:30 +0100

    fix avatar preview in profile screen for users not having all equipments

--- lua/modules/profile.lua
+++ lua/modules/profile.lua
@@ -111,7 +111,7 @@ profile.create = function(_, config)
 	end
 
 	-- avatarNode
-	local avatarNode = require("ui_avatar"):get(username, nil, {}, ui)
+	local avatarNode = uiAvatar:get(username, nil, nil, ui)
 	if DEBUG then
 		avatarNode.color = DEBUG_FRAME_COLOR
 	end
