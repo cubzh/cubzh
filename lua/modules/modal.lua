088fc49e693a510e4500e18ada739aebd5eae5ee
Adrien Duermael <adrien@duermael.com>
2024-01-15 12:53:40 +0100

    modal: idealReducedContentSize now provides minWidth

--- lua/modules/modal.lua
+++ lua/modules/modal.lua
@@ -143,7 +143,7 @@ modal.createContent = function(_)
 		-- (used to better fit grids for example)
 		-- The callback receives the size that's about to be applied
 		-- and should return the same size or smaller one.
-		idealReducedContentSize = nil, -- function(content, width, height)
+		idealReducedContentSize = nil, -- function(content, width, height, minWidth)
 
 		-- called right after modal content did become active
 		didBecomeActive = nil, -- function(content)
@@ -802,9 +802,11 @@ modal.create = function(_, content, maxWidth, maxHeight, position, uikit)
 		local contentSize = backgroundSize
 			- Number2(theme.padding * 2, (theme.padding * 2) + self.topBar.Height + self.bottomBar.Height)
 
+		local minWidth = math.max(totalTopWidth, totalBottomWidth)
+
 		if modalContent.idealReducedContentSize ~= nil then
 			local reducedContentSize =
-				modalContent.idealReducedContentSize(self._content, contentSize.Width, contentSize.Height)
+				modalContent.idealReducedContentSize(self._content, contentSize.Width, contentSize.Height, minWidth)
 			if reducedContentSize ~= nil and reducedContentSize ~= contentSize then
 				if reducedContentSize.X < totalTopWidth then
 					reducedContentSize.X = totalTopWidth
