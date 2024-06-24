781b69e7aec9321d9a4fbf67d909418c9b0bddf6
Arthur Cormerais <arthur@voxowl.com>
2024-06-24 19:26:08 +0200

    shaders : add 9-slice unpack flag

--- shaders/include/quad_lib.sh
+++ shaders/include/quad_lib.sh
@@ -3,22 +3,25 @@
 
 #include "./config.sh"
 
-void unpackQuadFullMetadata(float f, out float metadata[5]) {
+void unpackQuadFullMetadata(float f, out float metadata[6]) {
 	float unpack = f;
-	float b = floor((unpack + UNPACK_FUDGE) / 8192.0);
-	unpack -= b * 8192.0;
-	float g = floor((unpack + UNPACK_FUDGE) / 512.0);
-	unpack -= g * 512.0;
-	float r = floor((unpack + UNPACK_FUDGE) / 32.0);
-	unpack -= r * 32.0;
-	float s = floor((unpack + UNPACK_FUDGE) / 2.0);
-	float unlit = unpack - s * 2.0;
+	float b = floor((unpack + UNPACK_FUDGE) / 16384.0);
+	unpack -= b * 16384.0;
+	float g = floor((unpack + UNPACK_FUDGE) / 1024.0);
+	unpack -= g * 1024.0;
+	float r = floor((unpack + UNPACK_FUDGE) / 64.0);
+	unpack -= r * 64.0;
+	float s = floor((unpack + UNPACK_FUDGE) / 4.0);
+	unpack -= s * 4.0;
+	float unpack9SliceNormal = floor((unpack + UNPACK_FUDGE) / 2.0);
+	float unlit = unpack - unpack9SliceNormal * 2.0;
 
 	metadata[0] = unlit;
-	metadata[1] = s / VOXEL_LIGHT_MAX;
-	metadata[2] = r / VOXEL_LIGHT_MAX;
-	metadata[3] = g / VOXEL_LIGHT_MAX;
-	metadata[4] = b / VOXEL_LIGHT_MAX;
+	metadata[1] = unpack9SliceNormal;
+	metadata[2] = s / VOXEL_LIGHT_MAX;
+	metadata[3] = r / VOXEL_LIGHT_MAX;
+	metadata[4] = g / VOXEL_LIGHT_MAX;
+	metadata[5] = b / VOXEL_LIGHT_MAX;
 }
 
 float sliceUV(float uv, vec2 borders, float slice) {
