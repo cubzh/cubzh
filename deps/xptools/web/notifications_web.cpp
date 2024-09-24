ca3ee92d5694494f9751e9caf50aeffcb7f426c8
Gaëtan de Villèle <gdevillele@proton.me>
2024-09-24 10:06:47 +0200

    wasm : fix compilation

--- deps/xptools/web/notifications_web.cpp
+++ deps/xptools/web/notifications_web.cpp
@@ -1,27 +1,27 @@
 
 #include "notifications.hpp"
 
-bool vx::notification::notificationsAvailable() {
-    // Notifications are not implemented yet for this platform.
-    return false;
+namespace vx {
+namespace notification {
+
+NotificationAuthorizationStatus remotePushAuthorizationStatus() {
+    return NotificationAuthorizationStatus_NotSupported;
 }
 
-bool vx::notification::shouldShowInfoPopup() {
-    // Notifications are not implemented yet for this platform.
-    return false;
-}
+void requestRemotePushAuthorization(AuthorizationRequestCallback callback) {}
 
-void vx::notification::requestRemotePush() {
-    // Notifications are not implemented yet for this platform.
-}
+void requestRemotePushAuthorizationIfAuthStatusNotDetermined(AuthorizationRequestCallback callback) {}
 
-bool vx::notification::scheduleAllLocalReminders(const std::string& title,
-                                                 const std::string& message) {
-    // TODO: implement me!
-    return false;
-}
+void scheduleLocalNotification(
+    const std::string& title,
+    const std::string& body,
+    const std::string& identifier,
+    int days,
+    int hours,
+    int minutes,
+    int seconds) {}
 
-bool vx::notification::cancelAllLocalReminders() {
-    // TODO: implement me!
-    return false;
+void cancelLocalNotification(const std::string &identifier) {}
+
+}
 }
