0b05ba02cdfcafbf4c3a10bbcdc5586c205905b1
Adrian Duermael <adrien@cu.bzh>
2024-10-24 13:39:45 +0200

    fix crash in WSConnection.mm

--- deps/xptools/ios-macos/WSConnection.mm
+++ deps/xptools/ios-macos/WSConnection.mm
@@ -27,13 +27,14 @@
 @synthesize task = _task;
 @synthesize conn = _conn;
 
-- (instancetype)init {
-    return [self initWithWSConnection:nil];
-}
-
 - (void)dealloc {
-    [self.task cancel];
-    [self.session invalidateAndCancel];
+    if (self.task) {
+        [self.task cancel];
+    }
+    if (self.session) {
+        [self.session invalidateAndCancel];
+    }
+    self.conn = nil;
 }
 
 - (instancetype)initWithWSConnection:(vx::WSConnection*)conn {
@@ -50,24 +51,29 @@
     return self;
 }
 
+-(void)_close { // synced, to be called within main queue
+    if (self.task) {
+        // [self.task cancel];
+        [self.task cancelWithCloseCode:NSURLSessionWebSocketCloseCodeNormalClosure reason:nil];
+    }
+    if (self.session) {
+        [self.session invalidateAndCancel];
+    }
+    self.task = nil;
+    self.session = nil;
+}
+
 -(void)close {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (self.task) {
-            // [self.task cancel];
-            [self.task cancelWithCloseCode:NSURLSessionWebSocketCloseCodeNormalClosure reason:nil];
-        }
-        if (self.session) {
-            [self.session invalidateAndCancel];
-        }
-        self.task = nil;
-        self.session = nil;
+        [self _close];
+        self.conn = nil;
     });
 }
 
 -(void)connect {
-    [self close];
     dispatch_async(dispatch_get_main_queue(), ^{
         // NSLog(@"WebSocket connection CONNECT");
+        [self _close]; // close previous connection if necessary
         NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
         self.session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
         self.task = [self.session webSocketTaskWithURL:self.url];
@@ -76,24 +82,27 @@
 }
 
 - (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didOpenWithProtocol:(NSString *)protocol {
-    NSLog(@"WebSocket connection ESTABLISHED");
-
-    [self conn]->established();
-
-    std::shared_ptr<vx::ConnectionDelegate> delegate = [self conn]->getDelegate().lock();
-    if (delegate != nullptr) {
-        delegate->connectionDidEstablish(*[self conn]);
-    }
-
-    [self readNextMessage];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"WebSocket connection ESTABLISHED");
+        if (self.conn == nil) { return; }
+        self.conn->established();
+        std::shared_ptr<vx::ConnectionDelegate> delegate = [self conn]->getDelegate().lock();
+        if (delegate != nullptr) {
+            delegate->connectionDidEstablish(*[self conn]);
+        }
+        [self readNextMessage];
+    });
 }
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
-    if (error) {
-        // NSLog(@"WebSocket connection task failed");
-        NSLog(@"WebSocket connection task failed with error: %@", error);
-        [self conn]->closeOnError();
-    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (error) {
+            // NSLog(@"WebSocket connection task failed");
+            NSLog(@"WebSocket connection task failed with error: %@", error);
+            if (self.conn == nil) { return; }
+            self.conn->closeOnError();
+        }
+    });
 }
 
 - (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didCloseWithCode:(NSURLSessionWebSocketCloseCode)closeCode reason:(NSData *)reason {
@@ -113,53 +122,56 @@
 - (void)readNextMessage {
     dispatch_async(dispatch_get_main_queue(), ^{
         [self.task receiveMessageWithCompletionHandler:^(NSURLSessionWebSocketMessage * _Nullable message, NSError * _Nullable error) {
-            if (error) {
-                // NSLog(@"WebSocket connection ERROR receiving message: %@", error);
-                // connection already closed within `didCompleteWithError`
-                // self.conn->closeOnError();
-                return;
-            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (self.conn == nil) { return; }
 
-            if (message) {
-                std::shared_ptr<vx::ConnectionDelegate> delegate = self.conn->getDelegate().lock();
-                if (delegate != nullptr) {
-                    if (message.type == NSURLSessionWebSocketMessageTypeData) {
-                        // NSLog(@"WebSocket connection received message (DATA)");
-                        NSData *data = message.data;
-                        const char *bytes = reinterpret_cast<const char *>([data bytes]);
-                        NSUInteger length = [data length];
+                if (error) {
+                    // NSLog(@"WebSocket connection ERROR receiving message: %@", error);
+                    // connection already closed within `didCompleteWithError`
+                    // self.conn->closeOnError();
+                    return;
+                }
 
-                        if (bytes != nullptr && length > 0) {
-                            char *buffer = static_cast<char *>(malloc(length));
-                            memcpy(buffer, bytes, length);
+                if (message) {
+                    std::shared_ptr<vx::ConnectionDelegate> delegate = self.conn->getDelegate().lock();
+                    if (delegate != nullptr) {
+                        if (message.type == NSURLSessionWebSocketMessageTypeData) {
+                            // NSLog(@"WebSocket connection received message (DATA)");
+                            NSData *data = message.data;
+                            const char *bytes = reinterpret_cast<const char *>([data bytes]);
+                            NSUInteger length = [data length];
 
-                            vx::WSConnection::Payload_SharedPtr pld = vx::WSConnection::Payload::decode(buffer, length);
-                            pld->step("WSConnection::receivedBytes");
-                            delegate->connectionDidReceive(*self.conn, pld);
-                        } else {
-                            vxlog_error("[WSConnection::receivedBytes] dropped bytes");
-                        }
+                            if (bytes != nullptr && length > 0) {
+                                char *buffer = static_cast<char *>(malloc(length));
+                                memcpy(buffer, bytes, length);
 
-                    } else if (message.type == NSURLSessionWebSocketMessageTypeString) {
-                        // NSLog(@"WebSocket connection received message (STRING): %@", message.string);
-                        NSString *text = message.string;
-                        const char *utf8String = [text UTF8String];
+                                vx::WSConnection::Payload_SharedPtr pld = vx::WSConnection::Payload::decode(buffer, length);
+                                pld->step("WSConnection::receivedBytes");
+                                delegate->connectionDidReceive(*self.conn, pld);
+                            } else {
+                                vxlog_error("[WSConnection::receivedBytes] dropped bytes");
+                            }
 
-                        if (utf8String != nullptr) {
-                            size_t length = strlen(utf8String);
-                            char *buffer = static_cast<char *>(malloc(length));
-                            memcpy(buffer, utf8String, length);
+                        } else if (message.type == NSURLSessionWebSocketMessageTypeString) {
+                            // NSLog(@"WebSocket connection received message (STRING): %@", message.string);
+                            NSString *text = message.string;
+                            const char *utf8String = [text UTF8String];
 
-                            vx::WSConnection::Payload_SharedPtr pld = vx::WSConnection::Payload::decode(buffer, length);
-                            pld->step("WSConnection::receivedBytes");
-                            delegate->connectionDidReceive(*self.conn, pld);
-                        } else {
-                            vxlog_error("[WSConnection::receivedBytes] dropped bytes");
+                            if (utf8String != nullptr) {
+                                size_t length = strlen(utf8String);
+                                char *buffer = static_cast<char *>(malloc(length));
+                                memcpy(buffer, utf8String, length);
+
+                                vx::WSConnection::Payload_SharedPtr pld = vx::WSConnection::Payload::decode(buffer, length);
+                                pld->step("WSConnection::receivedBytes");
+                                delegate->connectionDidReceive(*self.conn, pld);
+                            } else {
+                                vxlog_error("[WSConnection::receivedBytes] dropped bytes");
+                            }
                         }
                     }
                 }
-            }
-
+            });
             [self readNextMessage];
         }];
     });
@@ -196,18 +208,10 @@ void WSConnection::_writePayload(const Payload_SharedPtr& p) {
 }
 
 void WSConnection::_close() {
-    if (_platformObject == nullptr) {
-        return;
-    }
-    [(__bridge WebSocketConnection*)_platformObject close];
     _detachPlatformObject();
 }
 
 void WSConnection::_destroy() {
-    if (_platformObject == nullptr) {
-        return;
-    }
-    [(__bridge WebSocketConnection*)_platformObject close];
     _detachPlatformObject();
 }
 
@@ -222,6 +226,7 @@ void WSConnection::_detachPlatformObject() {
     if (_platformObject == nullptr) {
         return;
     }
+    [(__bridge WebSocketConnection*)_platformObject close];
     CFRelease(_platformObject);
     _platformObject = nullptr;
 }
