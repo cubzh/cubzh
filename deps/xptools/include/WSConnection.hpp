7377579913d1369badede8adce44ff4d548d6365
Gaëtan de Villèle <gdevillele@proton.me>
2024-11-04 08:45:12 +0100

    xptools : code cleanup

--- deps/xptools/include/WSConnection.hpp
+++ deps/xptools/include/WSConnection.hpp
@@ -34,26 +34,26 @@ typedef std::weak_ptr<WSConnection> WSConnection_WeakPtr;
 
 class WSConnection final : public Connection {
 public:
-    
+
     static WSConnection_SharedPtr make(const std::string& scheme,
                                        const std::string& addr,
                                        const uint16_t& port);
-    
+
     ~WSConnection();
-    
+
     Status getStatus() override;
-    
+
     void reset() override;
     void close() override;
     void closeOnError() override;
-    
+
     // sets Status == OK
     // called by WSService
     void established();
 
     bool isClosed() override;
     void connect() override;
-    
+
     // accessors
     const std::string& getHost() const;
     const uint16_t& getPort() const;
@@ -70,64 +70,64 @@ public:
     /// notify the connection that it received data
     /// bytes must be copied!
     void receivedBytes(char *bytes, const size_t len, const bool isFinalFragment);
-    
+
     ///
     void setIsWriting(const bool isWriting);
-    
+
     ///
     bool isWriting();
-    
+
     // ------------------
     // CONNECTION WRITER
     // ------------------
-    
+
     /// Pushes Payload to be written
     void pushPayloadToWrite(const Payload_SharedPtr& p) override;
-    
+
     // Writes as much as possible in given buffer
     // Returns size written
     size_t write(char *buf, size_t len, bool& isFirstFragment, bool& partial) override;
-    
+
     bool doneWriting() override;
-    
+
 private:
-    
+
     /// private constructor
     WSConnection();
-    
+
     // Private methods
-    
+
     ///
     void init(const WSConnection_SharedPtr& ref,
               const std::string& scheme,
               const std::string& addr,
               const uint16_t& port);
-    
+
     ///
     void _threadFunction();
-    
+
     ///
     bool _isClosedNoMutex();
-    
+
     /// Returns payload to write,
     /// loading next one in line if needed.
     /// Returns NULL if there's nothing to write.
     Connection::Payload_SharedPtr _getPayloadToWrite();
-    
+
     // Private fields
-    
+
     ///
     WSConnection_WeakPtr _weakSelf;
-    
+
     /// address of server to connect to
     std::string _serverAddr;
-    
+
     /// server port to connect to
     uint16_t _serverPort;
-    
+
     /// indicate whether SSL is used
     bool _secure;
-    
+
     /// Indicates wether the connection is closed
     Status _status;
     std::mutex _statusMutex;
