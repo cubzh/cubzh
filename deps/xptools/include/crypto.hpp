38c3749527dd03ee7a25bf1084a8d3a584afbeed
Gaëtan de Villèle <gdevillele@proton.me>
2025-01-08 10:51:04 +0100

    xptools: add crc32 function

--- deps/xptools/include/crypto.hpp
+++ deps/xptools/include/crypto.hpp
@@ -15,10 +15,18 @@
 namespace vx {
 namespace crypto {
 
-///
+// Random
+
 std::string generateRandomHex(const uint16_t length);
+
+// UUID
+
 std::string generate_uuid_v4();
 std::string empty_uuid_v4();
+
+// Checksum
+
+uint32_t crc32(const uint32_t crc, const void * const buf, const uint32_t len);
 
 }
 }
