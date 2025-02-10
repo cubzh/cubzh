4ce00e4c26ca05af8b41de10ffec1177ac1663ab
Gaëtan de Villèle <gdevillele@proton.me>
2025-02-10 22:56:52 +0100

    luau : remove sources

--- deps/luau/CLI/src/ReplEntry.cpp
+++
@@ -1,7 +0,0 @@
-// This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
-#include "Luau/Repl.h"
-
-int main(int argc, char** argv)
-{
-    return replMain(argc, argv);
-}
