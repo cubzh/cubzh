d1bb1403546cd689e5cd54a30906ac7dcb132c7b
Adrian Duermael <adrien@cu.bzh>
2025-02-14 16:43:29 -0800

    hierarchyactions module: call Object:Recurse

--- lua/modules/hierarchyactions.lua
+++ lua/modules/hierarchyactions.lua
@@ -1,35 +1,9 @@
---[[
-    Example:
-
-Client.OnStart = function()
-	hierarchyActions = require("hierarchyactions")
-
-    -- get a list of all descendants including the root shape
-    local shapes = {}
-	hierarchyActions:applyToDescendants(shape,  { includeRoot = true }, function(s)
-		table.insert(shapes, s)
-	end)
-
-    -- get the number of blocks in children
-    local count = 0
-    hierarchyActions:applyToDescendants(shape, { depth = 1 }, function(s)
-		count = count + s.BlocksCount
-    end)
-
-    -- set a value for all descendants
-    hierarchyActions:applyToDescendants(shape, function(s)
-        s.PrivateDrawMode = drawMode
-    end)
-end
---]]
-
--- DEPRECATED (february 2025)
+-- DEPRECATED: USE Object:Recurse(callback, options) directly
 local hierarchyActions = {}
 local hierarchyActionsMetatable = {
 	__index = {
 		_maxDepth = -1,
 		applyToDescendants = function(self, shape, options, callback)
-			print("hierarchyActions is deprecated, please use object:Recurse(callback, options) instead")
 			if callback == nil then
 				callback = options
 				options = {}
@@ -37,24 +11,7 @@ local hierarchyActionsMetatable = {
 			if options.depth == nil then
 				options.depth = self._maxDepth
 			end
-			if options.includeRoot then
-				callback(shape)
-			end
-			if options.depth == 0 then
-				return
-			end
-			local newDepth = options.depth
-			if newDepth ~= self._maxDepth then
-				newDepth = newDepth - 1
-			end
-			local newOptions = {
-				depth = newDepth,
-				includeRoot = true, -- always include root for descendants
-			}
-			for i = 1, shape.ChildrenCount do
-				local child = shape:GetChild(i)
-				self:applyToDescendants(child, newOptions, callback)
-			end
+			shape:Recurse(callback, options)
 		end,
 	},
 }
