3a34ad7551f4356d71f2bba62f52a1842f61091d
Adrian Duermael <adrien@cu.bzh>
2025-02-22 21:19:55 -0800

    prevent error in item_grid (we should still find the root cause)

--- lua/modules/item_grid.lua
+++ lua/modules/item_grid.lua
@@ -464,6 +464,14 @@ itemGrid.create = function(_, config)
 											return
 										end
 
+										if cell.object == nil then 
+											-- should not happen... but in fact it does. 
+											-- request should be cancelled when cell is removed,
+											-- so the callback should not be triggered.
+											-- Let's debug this... but in the meantime, let's early return to avoid error.
+											return 
+										end
+
 										local thumbnail = ui:frame({ image = img })
 										thumbnail:setParent(cell)
 										thumbnail.Width = cell.Width - theme.paddingTiny * 2
