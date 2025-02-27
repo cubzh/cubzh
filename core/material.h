fd2272796268f065fa81cfb6b82c2c512103b1e3
Arthur Cormerais <arthur@voxowl.com>
2025-02-27 20:06:58 +0100

    engine + shaders : texture + GLTF support

--- core/material.h
+++ core/material.h
@@ -14,6 +14,7 @@ extern "C" {
 #include <stdint.h>
 
 typedef struct _Material Material;
+typedef struct _Texture Texture;
 
 typedef enum {
     AlphaMode_Opaque,
@@ -21,13 +22,21 @@ typedef enum {
     AlphaMode_Blend
 } AlphaMode;
 
+typedef enum {
+    MaterialTexture_Albedo,
+    MaterialTexture_Normal,
+    MaterialTexture_Metallic,
+    MaterialTexture_Emissive
+#define MaterialTexture_Count 4
+} MaterialTexture;
+
 Material* material_new(void);
 void material_free(Material* m);
-bool material_retain(const Material* m);
+bool material_retain(Material* m);
 void material_release(Material* m);
 
-void material_set_diffuse(Material* m, const uint32_t rgba);
-uint32_t material_get_diffuse(const Material* m);
+void material_set_albedo(Material* m, const uint32_t rgba);
+uint32_t material_get_albedo(const Material* m);
 void material_set_metallic(Material* m, const float value);
 float material_get_metallic(const Material* m);
 void material_set_roughness(Material* m, const float value);
@@ -42,6 +51,8 @@ void material_set_double_sided(Material* m, const bool value);
 bool material_is_double_sided(const Material* m);
 void material_set_unlit(Material* m, const bool value);
 bool material_is_unlit(const Material* m);
+void material_set_texture(Material* m, MaterialTexture slot, Texture* texture);
+Texture* material_get_texture(const Material* m, MaterialTexture slot);
 
 #ifdef __cplusplus
 }
